5db04ff1e9f33604cab4c0479654d7bb5b3fefcf
Anton Pustovidko <falli_ot@Antons-MBP.mshome.net>
2022-05-22 19:05:17 +0200

    add new project files

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+    for i in range(4) :
+        print_hi(i)
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
